1a4cf312f46d99240b22a76e6773a7986d315525
Ubuntu <ubuntu@ip-172-31-35-111.ec2.internal>
2019-10-17 14:45:44 +0000

    Login route decorator added to help create improved level of authorization for user access to website

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 def index(request):
@@ -7,7 +8,8 @@ def index(request):
     Return the index html file
     """
     return render(request, 'index.html')
-    
+
+@login_required    
 def logout(request):
     """
     Log the user out
@@ -20,6 +22,8 @@ def login(request):
     """
     Return a Login page
     """
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
         
@@ -30,11 +34,14 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in.")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect.")
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {"login_form": login_form})
+    
+    
     
     
     
