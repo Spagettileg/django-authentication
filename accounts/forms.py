c5a282727a700495e0a79fae72ea6d41bd54e9a4
Ubuntu <ubuntu@ip-172-31-35-111.ec2.internal>
2019-10-17 11:17:55 +0000

    Applying authentication and authorisation process to app

---
+++ accounts/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class UserLoginForm(forms.Form):
+    """
+    Form to be used to log users input
+    """
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
 
 def index(request):
     """
@@ -14,4 +15,26 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have been successfully logged out")
     return redirect(reverse('index'))
+    
+def login(request):
+    """
+    Return a Login page
+    """
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+                                     
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in.")
+            else:
+                login_form.add_error(None, "Your username or password is incorrect.")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {"login_form": login_form})
+    
+    
     
