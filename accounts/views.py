e7cf6f18470b37e843059e6cbe8f9f7221b19e76
Ubuntu <ubuntu@ip-172-31-35-111.ec2.internal>
2019-10-18 16:26:14 +0000

    User registration process added to include email, username, password verification with double password entry. Data entry is subjedt to django validationerror checks

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 def index(request):
     """
@@ -36,10 +36,46 @@ def login(request):
                 messages.success(request, "You have successfully logged in.")
                 return redirect(reverse('index'))
             else:
-                login_form.add_error(None, "Your username or password is incorrect.")
+                login_form.add_error
+                (None, "Your username or password is incorrect.")
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {"login_form": login_form})
+    
+def registration(request):
+    """
+    Render the registration page
+    """
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+        
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+        
+        if registration_form.is_valid():
+            registration_form.save()
+        
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+    
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+            else:
+                messages.error(
+                    request, "Unable to register your account at this time")
+    else:
+        # Create an instance of reg form
+        registration_form = UserRegistrationForm()  
+        
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
+    """
+    registration_form passed through as a context dictionary.
+    """
+    
+    
     
     
     
