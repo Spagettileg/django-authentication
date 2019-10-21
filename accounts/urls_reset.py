737353063918df040a6c2a250a13fc9e84d2c405
Ubuntu <ubuntu@ip-172-31-35-111.ec2.internal>
2019-10-21 15:17:33 +0000

    urls.py file created in accounts folder to house all the accounts related URL's

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -1,15 +1,15 @@
-from django.urls.conf import url
+from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.views import (password_reset, password_reset_done,
                                        password_reset_confirm,
                                        password_reset_complete)
 
 urlpatterns = [
-    url(r'^$',
+    url(r'^$', password_reset,
         {'post_reset_redirect': reverse_lazy('password_reset_done')},
         name='password_reset'),
     url(r'^done/$', password_reset_done, name='password_reset_done'),
-    url(r'^(?<uidb64>[0-9A-Za-z]+)-(?p<token>).+)/$', password_reset_confirm,
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?p<token>.+)/$', password_reset_confirm,
         {'post_reset_confirm': reverse_lazy('password_reset_complete')},
         name='password_reset_confirm'),
     url(r'^complete/$', password_reset_complete, name='password_reset_complete')
