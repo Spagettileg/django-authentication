41ecb263898e93670bc565464e38248094686911
Ubuntu <ubuntu@ip-172-31-35-111.ec2.internal>
2019-10-21 17:45:23 +0000

    Updated password reset files

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -9,8 +9,8 @@ urlpatterns = [
         {'post_reset_redirect': reverse_lazy('password_reset_done')},
         name='password_reset'),
     url(r'^done/$', password_reset_done, name='password_reset_done'),
-    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?p<token>.+)/$', password_reset_confirm,
-        {'post_reset_confirm': reverse_lazy('password_reset_complete')},
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
+        {'post_reset_redirect': reverse_lazy('password_reset_complete')},
         name='password_reset_confirm'),
     url(r'^complete/$', password_reset_complete, name='password_reset_complete')
     ]
