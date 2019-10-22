efdd40b3e5bb8bc4f92814c48b6300bc458a82d6
Ubuntu <ubuntu@ip-172-31-35-111.ec2.internal>
2019-10-22 19:41:58 +0000

    Updated files on password reset function

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -124,7 +124,7 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = [os.getenv("EMAIL_ADDRESS")]
+EMAIL_HOST_PASSWORD = [os.getenv("EMAIL_PASSWORD")]
 EMAIL_PORT = 587
 
